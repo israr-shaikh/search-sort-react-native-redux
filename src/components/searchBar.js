ca50d353f3ad0fa39cacc313eece1f71a42ebc03
Israr Shaikh <shaikh.coder20@gmail.com>
2020-06-15 14:34:31 +0530

    Update searchBar.js
    Icon was replaced

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,41 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
   ActivityIndicator,
-  Text,
   TextInput,
   TouchableHighlight,
   View,
-} from 'react-native';
-import {connect} from 'react-redux';
-import {searchList, sortList} from '../actions/users';
-import Styles from '../config/stylesheet';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { connect } from "react-redux";
+import { searchList, sortList } from "../actions/users";
+import Styles from "../config/stylesheet";
 
 class searchBar extends Component {
   render() {
-    let {isFetching, text, sort} = this.props.users;
+    let { isFetching, text, sort } = this.props.users;
     return (
       <View style={Styles.searchBarContainer}>
-        <View style={{flex: 0.85}}>
+        <View style={{ flex: 0.85 }}>
           <View style={Styles.searchBar}>
             <View style={Styles.searchBarLayout}>
               <View style={Styles.searchTextLayout}>
                 <Icon name="search" size={22} color="#B8B8B8" />
                 <TextInput
-                  onChangeText={text => this.props.searchList(text)}
+                  onChangeText={(text) => this.props.searchList(text)}
                   autoCapitalize="none"
                   value={text}
-                  placeholder={'Search User'}
-                  placeholderTextColor={'grey'}
+                  placeholder={"Search User"}
+                  placeholderTextColor={"grey"}
                   underlineColorAndroid="transparent"
                   style={Styles.searchBarInput}
                 />
 
-                <View style={{justifyContent: 'center'}}>
+                <View style={{ justifyContent: "center" }}>
                   {isFetching ? (
                     <ActivityIndicator
                       size="small"
-                      color={'black'}
+                      color={"black"}
                       animating={isFetching}
                     />
                   ) : null}
@@ -45,17 +44,31 @@ class searchBar extends Component {
           </View>
         </View>
         <TouchableHighlight
-          underlayColor={'#dedfe1'}
-          style={[Styles.sortContainer,{backgroundColor: sort == null ? '#dedfe1' : '#5fc9f8'}]}
-          onPress={() => this.props.sortList()}>
-          <Icon name="sort" size={35} color={sort == null ? 'grey' : 'white'} />
+          underlayColor={"#dedfe1"}
+          style={[
+            Styles.sortContainer,
+            { backgroundColor: sort == null ? "#dedfe1" : "#5fc9f8" },
+          ]}
+          onPress={() => this.props.sortList()}
+        >
+          <Icon
+            name={
+              sort == null
+                ? "sort"
+                : sort == true
+                ? "sort-amount-asc"
+                : "sort-amount-desc"
+            }
+            size={30}
+            color={sort == null ? "grey" : "white"}
+          />
         </TouchableHighlight>
       </View>
     );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     ...state,
   };
